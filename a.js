e364f2510a325586ba652edf22b41adca01fb102
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-15 17:43:30 +0700

    xong

---
+++ a.js
@@ -0,0 +1,15 @@
+// const a = console.log;
+// console.log(a);
+// a(1);
+
+function doSth(fn) {
+    fn(1);
+}
+
+doSth(console.log);
+
+// function getLog() {
+//     return console.log;
+// }
+
+// getLog()(1);
