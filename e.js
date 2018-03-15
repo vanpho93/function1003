def4bdb0e6831f994f3e0fe6cb004845d28a6692
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-15 18:44:13 +0700

    veHinh

---
+++ e.js
@@ -0,0 +1,85 @@
+function veHinh1(n) {
+    for(let i = 1; i <= n; i++) {
+        let str = '';
+        for(let j = 1; j <= n; j++) {
+            const dk = i >= j
+            str += dk ? '*' : ' ';
+        }
+        console.log(str);
+    }
+}
+
+function veHinh2(n) {
+    for(let i = 1; i <= n; i++) {
+        let str = '';
+        for(let j = 1; j <= n; j++) {
+            const dk = i + j >= n + 1;
+            str += dk ? '*' : ' ';
+        }
+        console.log(str);
+    }
+}
+
+function veHinh3(n) {
+    for(let i = 1; i <= n; i++) {
+        let str = '';
+        for(let j = 1; j <= n; j++) {
+            const dk = i + j <= n + 1;
+            str += dk ? '*' : ' ';
+        }
+        console.log(str);
+    }
+}
+
+function veHinh4(n) {
+    for(let i = 1; i <= n; i++) {
+        let str = '';
+        for(let j = 1; j <= n; j++) {
+            const soBuoc = (n - 1) / 2;
+            const soTrungTam = soBuoc + 1;
+            const soBuocDoc = Math.abs(j - soTrungTam);
+            const soBuocNgang = Math.abs(i - soTrungTam);
+            const dk = soBuocDoc + soBuocNgang <= soBuoc;
+            str += dk ? '*' : ' ';
+        }
+        console.log(str);
+    }
+}
+
+
+function veHinh5(n) {
+    for(let i = 1; i <= n; i++) {
+        let str = '';
+        for(let j = 1; j <= n; j++) {
+            const soBuoc = (n - 1) / 2;
+            const soTrungTam = soBuoc + 1;
+            const soBuocDoc = Math.abs(j - soTrungTam);
+            const soBuocNgang = Math.abs(i - soTrungTam);
+            const dkDuongVien = soBuocDoc + soBuocNgang === soBuoc;
+            const dkTrucDoc = i === soTrungTam;
+            const dkTrucNgang = j === soTrungTam;
+            const dk = dkDuongVien || dkTrucDoc || dkTrucNgang;
+            str += dk ? '*' : ' ';
+        }
+        console.log(str);
+    }
+}
+
+function veHinh6(n) {
+    const soCot = n * 2 - 1;
+    for(let i = 1; i <= n; i++) {
+        let str = '';
+        for(let j = 1; j <= soCot; j++) {
+            const soBuoc = n - 1;
+            const soTrungTam = soBuoc + 1;
+            const soBuocDoc = Math.abs(j - soTrungTam);
+            const soBuocNgang = Math.abs(i - soTrungTam);
+            const dk = soBuocDoc + soBuocNgang <= soBuoc;
+            str += dk ? '*' : ' ';
+        }
+        console.log(str);
+    }
+}
+
+
+veHinh6(5);
