e62d602c6436edd7662fee6349d13578fafeedd1
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-15 17:54:27 +0700

    functions

--- b.js
+++ b.js
@@ -1,5 +1,25 @@
-function inSoChan() {}
-function inSoLe() {}
-function inSoChia3Du1() {}
-function inSoChinhPhuong() {}
-// Math.sqrt() % 
+function inSoChan() {
+    for (let i = 0; i <= 100; i++) {
+        const dk = i % 2 === 0;
+        if (dk) console.log(i);
+    }
+}
+function inSoLe() {
+    for (let i = 0; i <= 100; i++) {
+        const dk = i % 2 === 1;
+        if (dk) console.log(i);
+    }
+}
+function inSoChia3Du1() {
+    for (let i = 0; i <= 100; i++) {
+        const dk = i % 3 === 1;
+        if (dk) console.log(i);
+    }
+}
+function inSoChinhPhuong() {
+    for (let i = 0; i <= 100; i++) {
+        const dk = Math.sqrt(i) % 1 === 0;
+        if (dk) console.log(i);
+    }
+}
+inSoChia3Du1();
